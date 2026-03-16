f3e31a8efb2bd3350026e245c9b691d68e519257
Shawkat Hossain Maruf <shawkath646@gmail.com>
2026-03-16 17:47:26 +0900

    Assignment solved!

--- assignment.py
+++ assignment.py
@@ -1,1 +1,32 @@
+# Solve 1
 
+width = int(input("Enter width: "))
+length = int(input("Enter length: "))
+
+area = width * length
+
+print(f"The area is {area}")
+
+# Solve 2
+
+integer_number = 10
+float_number = 4.256
+
+sum_number = integer_number + float_number
+
+print(f"Sum of integer and float is: {sum_number}")
+print(f"Data type is {type(sum_number)}")
+
+#Explain:  In python, if we sum integer and float, the sum of them will float
+
+# Solve 3
+
+input_name = input("Enter your name: ")
+input_age = int(input("Enter your age: "))
+input_gender = input("Enter your gender: ")
+input_height = int(input("Enter your height: "))
+
+print(f"Your name is: {input_name}")
+print(f"Your age is: {input_age}")
+print(f"Your gender is: {input_gender}")
+print(f"Your height is: {input_height}")
